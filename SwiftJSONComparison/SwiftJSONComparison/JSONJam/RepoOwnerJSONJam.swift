8f7e23c1338669d771de81a2c8e5768baf07053b
Harlan Kellaway <harlan@prolificinteractive.com>
2015-08-08 17:33:19 -0400

    Created RepoOwner model with JSON Jam

---
+++ SwiftJSONComparison/SwiftJSONComparison/JSONJam/RepoOwnerJSONJam.swift
@@ -0,0 +1,13 @@
+//
+//  RepoOwnerJSONJam.swift
+//  SwiftJSONComparison
+//
+//  Created by Harlan Kellaway on 8/8/15.
+//  Copyright (c) 2015 Harlan Kellaway. All rights reserved.
+//
+
+import UIKit
+
+class RepoOwnerJSONJam: JSONJam {
+   
+}
